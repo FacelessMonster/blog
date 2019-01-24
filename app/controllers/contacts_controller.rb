859b81880d2fec6adac7b3f023139b07c77d9e35
PearCreamPie <45975483+PearCreamPie@users.noreply.github.com>
2019-01-25 02:45:10 +0300

    save contects on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,11 @@ class ContactsController < ApplicationController
   def new
   end  
   def create
+    @contacts = Contact.create(params_contact)
   end  
+  private
+  def params_contact
+    params.require(:contact).permit(:email, :message)
+  end
+
 end
