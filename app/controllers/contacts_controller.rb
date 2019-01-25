681418932e4a7e6db217e6b8c97c3d52abf0ff4f
PearCreamPie <45975483+PearCreamPie@users.noreply.github.com>
2019-01-25 03:32:51 +0300

    add styles for validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,12 @@ class ContactsController < ApplicationController
   def new
   end  
   def create
-    @contacts = Contact.create(params_contact)
+    @contact = Contact.new(params_contact)
+    if @contact.valid?
+      @contact.save
+    else
+      render action: 'new'
+    end
   end  
   private
   def params_contact
