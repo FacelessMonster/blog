c12de9f3136bd65a567543f474b404f76a1369f6
PearCreamPie <45975483+PearCreamPie@users.noreply.github.com>
2019-01-30 03:10:25 +0300

    some mod

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ get 'about' => 'about#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resource :contacts, only: [:new, :create], path_names: {:new => ''}
 resources :articles do
-  resources :comments
+  resources :comments, only: [:create]
 end
 end
