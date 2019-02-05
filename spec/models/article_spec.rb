f23c61df7adc551107bf0bd0bd4f7a17b1ccacad
PearCreamPie <45975483+PearCreamPie@users.noreply.github.com>
2019-02-06 00:15:07 +0300

    why I do it?

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,6 +8,7 @@ describe Article, type: :model do
     it { should validate_presence_of :title}
     it { should validate_presence_of :content}
     it { should validate_length_of(:title).is_at_most(140) }
+    it { should validate_length_of(:content).is_at_most(4000) }
   end
 
   describe "#subject" do
