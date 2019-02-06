8f53ac76d9794eabaf0e166f251b608e283b8b20
PearCreamPie <45975483+PearCreamPie@users.noreply.github.com>
2019-02-06 17:17:16 +0300

    Update visitor_creates_account_spec.rb

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -3,14 +3,18 @@ require 'rails_helper'
 
 feature "account creation" do
   scenario "allows guest to create account" do
-    visit new_user_registration_path
-
-    fill_in 'user_email', with: 'blabla@bla.com'
-    fill_in 'user_password', with: 'qwerty121s'
-    fill_in 'user_password_confirmation', with: 'qwerty121s'
-    fill_in 'user_username', with: 'somename'
-
-    click_button "Sign up"
-    expect(page).to have_content "Welcome"
+    signed_up
+    expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 end  
+
+def signed_up
+  visit new_user_registration_path
+
+  fill_in 'user_email', with: 'blabla@bla.com'
+  fill_in 'user_password', with: 'qwerty121s'
+  fill_in 'user_password_confirmation', with: 'qwerty121s'
+  fill_in 'user_username', with: 'somename'
+
+  click_button "Sign up"
+end
