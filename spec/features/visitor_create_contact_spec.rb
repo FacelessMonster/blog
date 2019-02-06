e2cff6fb5b2646944476e91708303949fabe0682
PearCreamPie <45975483+PearCreamPie@users.noreply.github.com>
2019-02-06 14:48:46 +0300

    I18n.t in contacts + test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 feature "contact creation" do
-  scenario "allows to contact page" do
+  scenario "allows acces to contacts page" do
     visit '/contacts'
 
-    expect(page).to have_content "Contact with us" 
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
 end
