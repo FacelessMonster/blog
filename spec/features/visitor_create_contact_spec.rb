3e5f19a734071dfa485be49714631f783f03509a
PearCreamPie <45975483+PearCreamPie@users.noreply.github.com>
2019-02-06 16:10:07 +0300

    Update visitor_create_contact_spec.rb

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -8,4 +8,13 @@ feature "contact creation" do
 
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario "create message contact us" do
+    visit '/contacts'
+    fill_in "contact_email",	with: "blabla@bla.com"
+    fill_in "contact_message",	with: "sometext"
+    click_button "Send Message "
+
+    expect(page).to have_content "Ok ok"
+  end
 end
